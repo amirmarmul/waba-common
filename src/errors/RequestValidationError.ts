09a9bf80a06c881eba7c388750b2a02bf053076d
Amir Marmul <amiruddinmarmul@gmail.com>
2023-02-24 09:54:09 +0800

    class-validator support nested error

--- src/errors/RequestValidationError.ts
+++ src/errors/RequestValidationError.ts
@@ -11,9 +11,44 @@ export class RequestValidationError extends HttpError {
     }
 
     serializeErrors(): ErrorMessage[] {
-        return this.errors.map((error: ValidationError) => {
-            return { message: Object.values(error.constraints!).join(', '), field: error.property };
-        });
+        return this.errors
+            .flatMap((error) => this.mapChildrenToValidationErrors(error))
+            .filter((error) => !!error.constraints)
+            .flatMap((error) => {
+                return { message: Object.values(error.constraints!).join(', '), field: error.property };
+            });
+    }
+
+    private mapChildrenToValidationErrors(error: ValidationError, parentPath?: string): ValidationError[] {
+        if (!(error.children && error.children.length)) {
+            return [error];
+        }
+        const validationErrors = [];
+        parentPath = parentPath
+            ? `${parentPath}.${error.property}`
+            : error.property;
+        for (const item of error.children) {
+            if (item.children && item.children.length) {
+                validationErrors.push(
+                    ...this.mapChildrenToValidationErrors(item, parentPath),
+                );
+            }
+            validationErrors.push(
+                this.prependConstraintsWithParentProp(item, parentPath),
+            );
+        }
+        return validationErrors;
+    }
+
+    private prependConstraintsWithParentProp(error: ValidationError, parentPath?: string): ValidationError {
+        const constraints: any = {};
+        for (const key in error.constraints) {
+            constraints[key] = `${parentPath}.${error.constraints[key]}`;
+        }
+        return {
+            ...error,
+            constraints,
+        } as ValidationError;
     }
 }
 
