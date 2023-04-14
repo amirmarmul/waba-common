cee28b420264ccf742736ec18c0393fa58bdeb89
Amir Marmul <amiruddinmarmul@gmail.com>
2023-04-14 14:03:13 +0800

    add interceptor

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -6,5 +6,21 @@ export const axios = base.create({
     'Accept': 'application/json',
   }
 });
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response.data;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
+});
 curlirize(axios);
 export default axios;
