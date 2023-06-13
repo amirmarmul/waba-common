21bc90da47f324b673c6a93cd224e55c7e5f04c9
Amir Marmul <amiruddinmarmul@gmail.com>
2023-06-13 10:00:41 +0800

    add message template category update contract

---
+++ src/domain/MessageTemplateCategoryUpdate.ts
@@ -0,0 +1,9 @@
+export interface MessageTemplateCategoryUpdate {
+  sourceId: string;
+  sourceName: string;
+  sourceLanguage: string;
+  category: string;
+  categoryOld: string;
+}
+
+export default MessageTemplateCategoryUpdate;
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,6 +2,7 @@ export * from './TransactionMessaging';
 export * from './TransactionMessagingStatusUpdate';
 export * from './MessageTemplate';
 export * from './MessageTemplateStatusUpdate';
+export * from './MessageTemplateCategoryUpdate';
 export * from './ChannelWhatsapp';
 export * from './Payment';
 export * from './BalanceMutation';
