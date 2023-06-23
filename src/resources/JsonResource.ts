2972c011d507178515c5af2867750d9dace974e8
Amir Marmul <amiruddinmarmul@gmail.com>
2023-06-23 15:51:24 +0800

    interacts with attributes

--- src/resources/JsonResource.ts
+++ src/resources/JsonResource.ts
@@ -3,12 +3,45 @@ interface LooseObject {
 }
 
 export class JsonResource {
+  [property: string]: any;
+
   static hidden: string[] = [];
   static fillable: string[] = [];
   protected attributes: LooseObject = {};
 
   constructor(attributes: object) {
-    this.attributes = attributes;
+    this.fill(attributes);
+
+    return new Proxy(this, {
+      get: (target, property: string) => {
+        if (target.isAttribute(property)) {
+          return target.getAttribute(property);
+        }
+        return target[property];
+      },
+      set: (target, property: string, value) => {
+        target.setAttribute(property, value);
+        return true;
+      }
+    });
+  }
+
+  protected fill(attributes: LooseObject) {
+    for (const key in attributes) {
+      this.setAttribute(key, attributes[key]);
+    }
+  }
+
+  protected isAttribute(key: string): boolean {
+    return this.attributes.hasOwnProperty(key);
+  }
+  
+  protected getAttribute(key: string) {
+    return this.attributes[key];
+  }
+
+  protected setAttribute(key: string, value: any): void {
+    this.attributes[key] = value;
   }
 
   protected getHidden(): string[] {
