f726c414223595de1ab5ab9ad3a6bc66ee1641f5
Amir Marmul <amiruddinmarmul@gmail.com>
2023-08-16 21:59:42 +0800

    debug: default format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,7 +2,7 @@ import { createLogger, format, transports } from 'winston';
 
 export const logger = createLogger({
   level: process.env.LOG_LEVEL,
-  format: format.combine(format.timestamp(), format.splat(), format.json()),
+  // format: format.combine(format.timestamp(), format.splat(), format.json()),
   transports: [
     new transports.Console()
   ],
