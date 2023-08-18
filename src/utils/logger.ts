72aabb74c26ec0b2dfe195f4b08b9917e06bbd11
Amir Marmul <amiruddinmarmul@gmail.com>
2023-08-18 20:12:51 +0800

    add splat

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,8 @@
-import { createLogger, transports } from 'winston';
+import { createLogger, format, transports } from 'winston';
 
 export const logger = createLogger({
   level: process.env.LOG_LEVEL,
+  format: format.combine(format.json(), format.splat()),
   transports: [
     new transports.Console()
   ],
