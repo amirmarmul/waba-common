948e2b839f8227dd6fc8c1df24082ffe7a2fb295
muhammadhasanz <mhmmdhasanz@gmail.com>
2023-09-08 09:24:26 +0800

    refactor(domain): Change modifier props to protected

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -2,7 +2,7 @@ import { UniqueId } from '@/core/domain/UniqueId';
 
 export abstract class Entity<T> {
   protected readonly _id: UniqueId;
-  public readonly props: T;
+  protected readonly props: T;
 
   constructor(props: T, id?: UniqueId) {
     this._id = id ?? new UniqueId();
