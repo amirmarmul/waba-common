8dbdfedf99dbf9c8defd9487442c240f192f547c
Amir Marmul <amiruddinmarmul@gmail.com>
2024-06-10 11:13:13 +0800

    fix: add async keyword

--- src/core/utils/cache.ts
+++ src/core/utils/cache.ts
@@ -1,16 +1,16 @@
 import { Container } from '@/core/infrastructure/Container';
 import { Cache } from '@/core/domain/cache/Cache';
 
-export function cache<T>(key: string, callback: Function, ttl: number = 30) {
+export async function cache<T>(key: string, callback: Function, ttl: number = 30) {
   const cache = Container.get<Cache>('cacheService');
 
-  const value = cache.get(key);
+  const value = await cache.get(key);
   if (value) {
     return value;
   }
 
-  return callback().then((data: T) => {
-    cache.set(key, data, ttl);
+  return callback().then(async (data: T) => {
+    await cache.set(key, data, ttl);
     return data;
   });
 }
