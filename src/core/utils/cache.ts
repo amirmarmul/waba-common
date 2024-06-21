28735b8b91b8e0b2342236f93b550c9601f58d66
Amir Marmul <amiruddinmarmul@gmail.com>
2024-06-21 15:57:27 +0800

    bugfix: cache helper using the cache implementation previous version

--- src/core/utils/cache.ts
+++ src/core/utils/cache.ts
@@ -1,8 +1,8 @@
 import { Container } from '@/core/infrastructure/Container';
-import { Cache } from '@/core/infrastructure/cache/Cache';
+import { Cache } from '@/core/domain/cache/Cache';
 
 export async function cache<T>(key: string, callback: Function, ttl: number = 30) {
-  const cache = Container.get(Cache);
+  const cache = Container.get<Cache>('cacheService');
 
   const value = await cache.get(key);
   if (value) {
@@ -10,7 +10,7 @@ export async function cache<T>(key: string, callback: Function, ttl: number = 30
   }
 
   return callback().then(async (data: T) => {
-    await cache.put(key, data, ttl);
+    await cache.set(key, data, ttl);
     return data;
   });
 }
