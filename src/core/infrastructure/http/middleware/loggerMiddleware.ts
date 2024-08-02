771f04302ee5357af359681e6447de0ebb93e8dd
Amir Marmul <amiruddinmarmul@gmail.com>
2024-08-02 16:00:14 +0800

    feat: change morgan log format to short

--- src/core/infrastructure/http/middleware/loggerMiddleware.ts
+++ src/core/infrastructure/http/middleware/loggerMiddleware.ts
@@ -1,7 +1,7 @@
 import morgan from 'morgan';
 import { logger } from '@/core/utils/logger';
 
-export const loggerMiddleware = morgan('combined', {
+export const loggerMiddleware = morgan('short', {
   stream: new class {
     write(message: string) {
       logger.info(message);
