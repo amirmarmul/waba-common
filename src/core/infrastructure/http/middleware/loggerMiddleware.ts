03103e05047144ec7b056b34edf95e5d50c8e0ff
Amir Marmul <amiruddinmarmul@gmail.com>
2024-08-08 11:47:01 +0800

    feat: morgan json format

--- src/core/infrastructure/http/middleware/loggerMiddleware.ts
+++ src/core/infrastructure/http/middleware/loggerMiddleware.ts
@@ -1,7 +1,10 @@
 import morgan from 'morgan';
+import json from 'morgan-json';
 import { logger } from '@/core/utils/logger';
 
-export const loggerMiddleware = morgan('short', {
+const format = json('short', { stringify: false });
+
+export const loggerMiddleware = morgan(format, {
   stream: new class {
     write(message: string) {
       logger.info(message);
