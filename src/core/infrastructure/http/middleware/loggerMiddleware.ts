9d443df0675daa61bf444c1a6c5f78166af5031c
Amir Marmul <amiruddinmarmul@gmail.com>
2024-08-08 13:34:44 +0800

    feat: object ,eta

--- src/core/infrastructure/http/middleware/loggerMiddleware.ts
+++ src/core/infrastructure/http/middleware/loggerMiddleware.ts
@@ -2,12 +2,12 @@ import morgan from 'morgan';
 import json from 'morgan-json';
 import { logger } from '@/core/utils/logger';
 
-const format = json(':method :url :status :res[content-length] :response-time');
+const format = json(':method :url :status :res[content-length] :response-time', { stringify: false });
 
 export const loggerMiddleware = morgan(format, {
   stream: new class {
     write(message: any) {
-      logger.log('info', 'accesslog', { meta: message });
+      logger.info('accesslog', { meta: message });
     }
   }
 });
