e9af335acb3d6d900bc7e3f7b49298a2319c9190
Amir Marmul <amiruddinmarmul@gmail.com>
2024-08-08 13:57:02 +0800

    add user agent

--- src/core/infrastructure/http/middleware/loggerMiddleware.ts
+++ src/core/infrastructure/http/middleware/loggerMiddleware.ts
@@ -2,7 +2,7 @@ import morgan from 'morgan';
 import json from 'morgan-json';
 import { logger } from '@/core/utils/logger';
 
-export const loggerMiddleware = morgan(json(':method :url :status :res[content-length] :response-time'), {
+export const loggerMiddleware = morgan(json(':method :url :status :res[content-length] :response-time :user-agent'), {
   stream: new class {
     write(message: any) {
       const logMessage = JSON.parse(message);
