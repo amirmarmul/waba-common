443de74e546b03ba026ffaf1c8c146215b97cbac
m4nzm333 <irman.mashuri@gmail.com>
2024-12-11 10:08:53 +0800

    fix: not exporting DatabaseRepository interface

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,57 +1,58 @@
-export * from '@/core/domain/Entity';
-export * from '@/core/domain/Identifier';
-export * from '@/core/domain/UniqueId';
-export * from '@/core/domain/UseCase';
-export * from '@/core/domain/ValueObject';
+export * from "@/core/domain/Entity";
+export * from "@/core/domain/Identifier";
+export * from "@/core/domain/UniqueId";
+export * from "@/core/domain/UseCase";
+export * from "@/core/domain/ValueObject";
 
-export * from '@/core/domain/auth/Auth';
-export * from '@/core/domain/auth/Authenticable';
-export * from '@/core/domain/auth/Guard';
-export * from '@/core/domain/auth/TokenGuard';
-export * from '@/core/domain/auth/UserProvider';
+export * from "@/core/domain/auth/Auth";
+export * from "@/core/domain/auth/Authenticable";
+export * from "@/core/domain/auth/Guard";
+export * from "@/core/domain/auth/TokenGuard";
+export * from "@/core/domain/auth/UserProvider";
 
-export * from '@/core/infrastructure/http/App';
+export * from "@/core/infrastructure/http/App";
 
-export * from '@/core/infrastructure/events/Event';
-export * from '@/core/infrastructure/events/Listener';
-export * from '@/core/infrastructure/events/Worker';
+export * from "@/core/infrastructure/events/Event";
+export * from "@/core/infrastructure/events/Listener";
+export * from "@/core/infrastructure/events/Worker";
 
-export { Event as EventRpc } from '@/core/infrastructure/events/rpc/Event';
-export { Listener as ListenerRpc } from '@/core/infrastructure/events/rpc/Listener';
-export { Event as EventGrpc } from '@/core/infrastructure/events/grpc/Event';
-export { Listener as ListenerGrpc } from '@/core/infrastructure/events/grpc/Listener';
+export { Event as EventRpc } from "@/core/infrastructure/events/rpc/Event";
+export { Listener as ListenerRpc } from "@/core/infrastructure/events/rpc/Listener";
+export { Event as EventGrpc } from "@/core/infrastructure/events/grpc/Event";
+export { Listener as ListenerGrpc } from "@/core/infrastructure/events/grpc/Listener";
 
-export * from '@/core/infrastructure/Container';
-export * from '@/core/infrastructure/Controller';
-export * from '@/core/infrastructure/Mapper';
-export * from '@/core/infrastructure/Repo';
+export * from "@/core/infrastructure/Container";
+export * from "@/core/infrastructure/Controller";
+export * from "@/core/infrastructure/Mapper";
+export * from "@/core/infrastructure/Repo";
+export * from "@/core/infrastructure/DatabaseRepository";
 
-export * from '@/core/infrastructure/loaders/Loader';
-export * from '@/core/infrastructure/loaders/Manager';
+export * from "@/core/infrastructure/loaders/Loader";
+export * from "@/core/infrastructure/loaders/Manager";
 
-export * from '@/core/infrastructure/cache/Cache';
+export * from "@/core/infrastructure/cache/Cache";
 
 /**
  * @deprecated
  */
-export * from '@/core/infrastructure/cache/InMemoryCache';
-export * from '@/core/infrastructure/cache/RedisCache';
+export * from "@/core/infrastructure/cache/InMemoryCache";
+export * from "@/core/infrastructure/cache/RedisCache";
 
-export * from '@/core/errors/AppError';
-export * from '@/core/errors/AuthenticationTokenMissingError';
-export * from '@/core/errors/BadRequestError';
-export * from '@/core/errors/NotAuthorizedError';
-export * from '@/core/errors/NotFoundError';
-export * from '@/core/errors/RequestValidationError';
-export * from '@/core/errors/ValidationError';
-export * from '@/core/errors/WrongAuthenticationTokenError';
-export * from '@/core/errors/WrongCredentialsError';
+export * from "@/core/errors/AppError";
+export * from "@/core/errors/AuthenticationTokenMissingError";
+export * from "@/core/errors/BadRequestError";
+export * from "@/core/errors/NotAuthorizedError";
+export * from "@/core/errors/NotFoundError";
+export * from "@/core/errors/RequestValidationError";
+export * from "@/core/errors/ValidationError";
+export * from "@/core/errors/WrongAuthenticationTokenError";
+export * from "@/core/errors/WrongCredentialsError";
 
-export * from '@/core/utils/Hash';
-export * from '@/core/utils/Crypto';
-export * from '@/core/utils/commands';
-export * from '@/core/utils/events';
-export * from '@/core/utils/logger';
-export * from '@/core/utils/response';
-export * from '@/core/utils/validator';
-export * from '@/core/utils/cache';
+export * from "@/core/utils/Hash";
+export * from "@/core/utils/Crypto";
+export * from "@/core/utils/commands";
+export * from "@/core/utils/events";
+export * from "@/core/utils/logger";
+export * from "@/core/utils/response";
+export * from "@/core/utils/validator";
+export * from "@/core/utils/cache";
