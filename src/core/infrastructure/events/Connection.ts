3339ec614d482cfeef518f350ec4bcbb33b6fa59
Amir Marmul <amiruddinmarmul@gmail.com>
2024-12-17 09:24:44 +0800

    feat: support multiple type of connection

--- src/core/infrastructure/events/Connection.ts
+++ src/core/infrastructure/events/Connection.ts
@@ -1,22 +1,38 @@
-import logger from '@/core/utils/logger';
 import amqp, { AmqpConnectionManager, Channel, ChannelWrapper } from 'amqp-connection-manager';
+import logger from '@/core/utils/logger';
+
+type ConnectionType = 'publisher' | 'listener' | 'default';
 
 export { Channel, ChannelWrapper };
 
 export class Connection {
-  private static connection: AmqpConnectionManager;
+  private static connections: Record<ConnectionType, AmqpConnectionManager | null> = {
+    default: null,
+    listener: null,
+    publisher: null
+  };
 
   private constructor() {
-    Connection.connection = amqp.connect([process.env.APP_MQ!]);
-    Connection.connection.on('error', (err) => {
-      logger.error('AMQP connection error:', { msg: err.message, stack: err.stack });
-    });
+    //
   }
 
-  static getConnection(): AmqpConnectionManager {
-    if (!Connection.connection) {
-      new Connection();
-    }
-    return Connection.connection;
+  private static createConnection(connectionType: ConnectionType = 'default'): AmqpConnectionManager {
+    const connection = amqp.connect([process.env.APP_MQ!]);
+
+    connection.on('connect', () => {
+      logger.info('Connection established successfully.', { connectionType });
+    });
+
+    connection.on('disconnect', (params) => {
+      logger.warn('Connection lost. ', { connectionType, reason: params.err.message });
+      this.connections[connectionType] = null;
+    });
+
+    this.connections[connectionType] = connection;
+    return connection;
+  }
+
+  static getConnection(connectionType: ConnectionType = 'default'): AmqpConnectionManager {
+    return this.connections[connectionType] ?? this.createConnection(connectionType);
   }
 }
