4016bf8e814677e2f9b72eba3e48a258d4028bac
muhammadhasanz <mhmmdhasanz@gmail.com>
2025-03-06 10:59:52 +0800

    feat(mongo): Added the cursor pagination

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,6 @@
 export * from '@/shared/plugins/axios';
 export * from '@/shared/plugins/axios-logger-mongo';
 export * from '@/shared/plugins/mongoose';
+export * from '@/shared/plugins/cursor';
 
 export * from '@/shared/resources/JsonResource';
